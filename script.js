9b184a129bf6f5e6d8641532e35b1ba92dc7babb
astucioso <150879600+astucioso@users.noreply.github.com>
2024-12-16 02:49:53 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// script.js
+
+document.getElementById('revealButton').addEventListener('click', function() {
+    // Iniciar a música
+    var audio = document.getElementById('background-music');
+    audio.play();  // Inicia a reprodução da música
+
+    // Esconde o botão e texto
+    document.querySelector('.start-screen').style.display = 'none';
+
+    // Exibe os perfis
+    document.getElementById('profiles-container').style.display = 'flex';
+});
